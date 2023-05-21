0f155cbdbc8449a4fd5af82cec3d90c564d74158
PhanVu5 <96001569+PhanVu5@users.noreply.github.com>
2023-05-21 19:25:56 +0700

    Add files via upload

---
+++ config copy.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Em';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc Mừng sinh nhật công chúa của tôi';
+
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Cô gái dễ thương';
+const giftUrl = 'https://phanvu5.github.io/letter/';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc Linh 22/05 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau. Bấm tiếp vào ảnh nhé!';
+
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
